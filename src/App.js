b054e7a8097c43eeee71c9835ef56f47bb52c92d
neel patel <neelpatel@neels-MacBook-Pro.local>
2022-06-02 00:55:01 -0400

    conditional render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 function App(){
+    const title = 'Feedback';
+    const body = 'This is my blog post'
+    const comments = [
+        { id: 1, text: 'comment one' },
+        { id: 2, text: 'comment two' },
+        { id: 3, text: 'comment three' }
+    ]
+
+    const loading = false;
+    const showComments = true;
+
+    if(loading) return <h1>Loading...</h1>
+
     return (
         <div className='container'>
-            <h1>My App</h1>
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            {showComments && (
+                <div className="comments">
+                    <h3>Comments({comments.length})</h3>
+                    <ul>
+                        {comments.map((comment, index) => (
+                            <li key={index}>{comment.text}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) }
+
+
         </div>
     )
 }
