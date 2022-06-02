38f4f7f14c5e3d46389270c4e43b7f46bdadf79a
neel patel <neelpatel@neels-MacBook-Pro.local>
2022-06-01 23:56:49 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
+    const title = 'Feedback';
+    const body = 'This is my blog post'
+    const comments = [
+        { id: 1, text: 'comment one' },
+        { id: 2, text: 'comment two' },
+        { id: 3, text: 'comment three' }
+    ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className='container'>
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            <div className="comments">
+                <h3>Comments({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
