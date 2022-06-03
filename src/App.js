9cf9f0c5ced57726ef84734bac5b93c3d8854df7
neel patel <neelpatel@neels-MacBook-Pro.local>
2022-06-02 23:58:41 -0400

    header component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
+import Header from './components/Header';
+
 function App(){
-    const title = 'Feedback';
-    const body = 'This is my blog post'
-    const comments = [
-        { id: 1, text: 'comment one' },
-        { id: 2, text: 'comment two' },
-        { id: 3, text: 'comment three' }
-    ]
-
-    const loading = false;
-    const showComments = true;
-
-    if(loading) return <h1>Loading...</h1>
-
     return (
-        <div className='container'>
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            {showComments && (
-                <div className="comments">
-                    <h3>Comments({comments.length})</h3>
-                    <ul>
-                        {comments.map((comment, index) => (
-                            <li key={index}>{comment.text}</li>
-                        ))}
-                    </ul>
-                </div>
-            ) }
-
-
-        </div>
+        <>
+            <Header texts={null} />
+            <div className='container'>
+                <h1>My App</h1>
+            </div>
+        </>
     )
 }
 
