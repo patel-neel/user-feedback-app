f00b9367cb903d8202cebe03b6560d808317e613
neel patel <321neel@gmail.com>
2022-06-09 22:59:52 -0400

    state initialized

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function FeedbackItem() {
+  return (
+    <div className='card'>
+        <div className="num-display">10</div>
+        <div className="text-display">This is an example of a feedback Item
+        </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
